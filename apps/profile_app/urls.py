d637e28db17cdefd8f0612609c2cab766ff2640f
personalbuse <defaultuser965@gmail.com>
2025-11-02 10:17:17 -0500

    Update urls.py

--- apps/profile_app/urls.py
+++ apps/profile_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("/profileManagement/", include("apps.profile_app.subapps.profile.urls")),
+    path("profileManagement/", include("apps.profile_app.subapps.profile.urls")),
     #path("imageUpload/", include("apps.profile_app.subapps.imageupload.urls")),
 ]
